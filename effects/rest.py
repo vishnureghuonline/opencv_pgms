544672295be479df9d965970ab46eec052c8f8bb
Vishnu Reghu <vishnureghuonline@gmail.com>
2015-12-29 13:09:50 +0530

    Update rest.py

--- effects/rest.py
+++ effects/rest.py
@@ -12,9 +12,9 @@ mask = cv2.imread("mask.png")
 mask=cv2.resize(mask,(img.shape[1],img.shape[0]))
 im=np.copy(img)
 newImage=np.zeros(img.shape,np.uint8)
-img = cv2.fastNlMeansDenoisingColored(img,None,5,5,5,21)
+img = cv2.fastNlMeansDenoisingColored(img,None,4,4,4,21)
 i, j, k = img.shape
-R = img[...,2] * 1.0 + img[...,1] * 0.0+ img[...,0] * 0.0
+R = img[...,2] * 1.0 + img[...,1] * 0.1+ img[...,0] * 0.0
 G = img[...,2] * 0.0+ img[...,1] * 1.0 + img[...,0] * 0.0
 B = img[...,2] * 0.0 + img[...,1] * 0.0 + img[...,0] * 1.0
 R[R>255]=255
@@ -23,10 +23,10 @@ B[B>255]=255
 newImage[...,2] =R
 newImage[...,1] =G
 newImage[...,0] =B
-out=color(newImage,1.7)
-out=contrast(newImage,1.5)
-enhanced_image= cv2.addWeighted(mask,0.2,out,0.8, 0)
+out=sharp(newImage,1.9)
+out=contrast(newImage,1.4)
+
 cv2.imshow("real",im)
-cv2.imshow("effect1",enhanced_image)
-cv2.imwrite("final2.jpg",enhanced_image)
+cv2.imshow("effect1",out)
+cv2.imwrite("final2.jpg",out)
 cv2.waitKey(0)
